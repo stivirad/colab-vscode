183113ddc89c75f09721bf5c0b6514a693b85b8d
stivirad <stiviradovani@gmail.com>
2025-12-26 14:39:25 +0100

    Aggiornamenti locali

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Repo collegato correttamente")
+# CPU e RAM
+import psutil
+import platform
+
+print("Sistema operativo:", platform.system())
+print("CPU:", platform.processor())
+print("Core CPU:", psutil.cpu_count(logical=True))
+print("RAM totale (GB):", round(psutil.virtual_memory().total / 1e9, 2))
+
+# GPU NVIDIA
+!nvidia-smi
